328b9cc0a69b8df75bf701b876522843c82a808d
RobertoJunior17 <104389402+RobertoJunior17@users.noreply.github.com>
2023-06-09 00:17:12 -0300

    Add files via upload

---
+++ atv1.php
@@ -0,0 +1,30 @@
+<?php
+/*1. Faça um programa PHP que crie duas variáveis e atribua dois valores
+inteiros. Apresente todas as operações: adição, subtração, divisão,
+multiplicação e resto da divisão utilizando os operadores de atribuição.*/
+
+$vlr1 = 10;
+$vlr2 = 5;
+
+// Adição
+$resultado = $vlr1 + $vlr2;
+echo "Valor Adição: " . $resultado . "<br>";
+
+// Subtração 
+$resultado = $vlr1 - $vlr2;
+echo "Valor Subtração: " . $resultado . "<br>";
+
+// Divisão
+$resultado = $vlr1 / $vlr2;
+echo "Valor Divisão: " . $resultado . "<br>";
+
+// Multiplicação
+$resultado = $vlr1 * $vlr2;
+echo "Valor Multiplicação: " . $resultado . "<br>";
+
+// Resto da divisão (Módulo)
+$resultado = $vlr1 % $vlr2;
+echo "Valor do Resto da Divisão: " . $resultado . "<br>";
+
+
+?>
